cdeb6644dbb160f8d689c9234edc8b74913ff9e8
Sarah-Joy Roxborough <sarahjroxb@gmail.com>
2021-10-31 21:13:56 -0400

    Edited files for assignment

---
+++ index.js
@@ -0,0 +1,18 @@
+var btn = document.querySelector('button')
+var forecastEl = document.getElementById('forecast')
+
+
+btn.onclick = function() {
+    // console.log('clicked')
+    fetch('api.openweathermap.org/data/2.5/weather?q=YOURQUERY&units=imperial&appid=1f4b4a7690d5fdbf1db1b7cd9f01fdcb') // make the request
+    .then(function(res) {
+        return res.json() // when the request is received, convert to json
+    })
+    .then(function(res) {
+        console.log(res) // when the json is converted, log it
+        console.log('Here!') // note: this will log BEFORE fetch is finished
+        })
+}
+//.catch(function(err) {
+//    console.log(err)
+// })
